5480cb1f32d2ee8d1e0785112df8a24449f80c81
vinaygowda91 <58931205+vinaygowda91@users.noreply.github.com>
2019-12-21 14:27:04 +0530

    update

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -5,14 +5,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+uu
 public class WorksWithHerokuServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/plain");
         response.setStatus(404);
         PrintWriter writer = response.getWriter();
-        writer.print("Hi Ravigowda Welcome to Devops !!!!!! Hurry");
+        writer.print("Hi Ravigowda Welcome to Devops !!!!!! Hurry"); 
         writer.close();
     }
 }
