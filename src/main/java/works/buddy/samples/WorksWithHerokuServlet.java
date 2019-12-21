2df70b5a01fb38d2cc0943cabea81edfe9332a7c
vinaygowda91 <58931205+vinaygowda91@users.noreply.github.com>
2019-12-21 14:13:41 +0530

    update

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -13,7 +13,7 @@ public class WorksWithHerokuServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setStatus(404);
         PrintWriter writer = response.getWriter();
-        writer.print("Buddy Works with Heroku");
+        writer.print("Hi Ravigowda Welcome to Devops !!!!!! Hurry");
         writer.close();
     }
 }
